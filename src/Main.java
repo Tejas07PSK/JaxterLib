bf834416c323f56c2ec5ecce632feb888ee4f4b9
Palash Sarkar <palashsarkar0007@gmail.com>
2018-08-28 12:48:48 +0530

    Testing 'InsertionSort' algorithm class!!

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,11 @@ public class Main
     public static void main (String args []) throws Exception
     {
 
-        Float arr [] = {3.66f, 5.87f, 13.22f, -26.33f, 1.30f, 44.33f, 56.65f, -14.43f, 1.09f, 2.67f, 7.22f};
-        //long arr [] = {23L, 11L, 12L, 5L, 1L, -2L, 34L, 55L, 1L, 2L, 56L, 77L, 34L};
-        arr = BubbleSort.for_FloatArray(arr);
-        for (float i : arr)
+        //Float arr [] = {3.66f, 5.87f, 13.22f, -26.33f, 1.30f, 44.33f, 56.65f, -14.43f, 1.09f, 2.67f, 7.22f};
+        //Integer arr [] = {23, 11, 12, 5, 1, -2, 34, 55, 1, 2, 56, 77, 34};
+        double arr [] = {5.0, 4.0, 3.0, 2.0, 1.0};
+        arr = InsertionSort.for_doubleArray(arr);
+        for (double i : arr)
         {
 
             System.out.print(i + " ");
