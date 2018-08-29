44a576f031d6b18fea80cfb3a201fc5b06f5d4c1
Palash Sarkar <palashsarkar0007@gmail.com>
2018-08-29 13:33:27 +0530

    Testing 'MergeSort' algorithm class!!

--- src/Main.java
+++ src/Main.java
@@ -16,11 +16,11 @@ public class Main
     public static void main (String args []) throws Exception
     {
 
-        //Float arr [] = {3.66f, 5.87f, 13.22f, -26.33f, 1.30f, 44.33f, 56.65f, -14.43f, 1.09f, 2.67f, 7.22f};
-        //Integer arr [] = {23, 11, 12, 5, 1, -2, 34, 55, 1, 2, 56, 77, 34};
-        double arr [] = {5.0, 4.0, 3.0, 2.0, 1.0};
-        arr = InsertionSort.for_doubleArray(arr);
-        for (double i : arr)
+        float arr [] = {3.66f, 5.87f, 13.22f, -26.33f, 1.30f, 44.33f, 56.65f, -14.43f, 1.09f, 2.67f, 7.22f};
+        //int arr [] = {23, 11, 12, 5, 1, -2, 34, 55, 1, 2, 56};
+        //Double arr [] = {3.66, 5.87, 13.22, -26.33, 1.30, 44.33, 56.65, -14.43, 1.09, 2.67, 7.22};
+        arr = MergeSort.for_floatArray(arr);
+        for (float i : arr)
         {
 
             System.out.print(i + " ");
