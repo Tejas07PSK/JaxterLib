f16f7985b899b21981fa90689e973774c8314fe6
Palash Sarkar <palashsarkar0007@gmail.com>
2018-08-29 14:05:55 +0530

    Optimizations for 'MergeSort' algorithm class!!

--- src/com/lib/algo/MergeSort.java
+++ src/com/lib/algo/MergeSort.java
@@ -18,18 +18,21 @@ public class MergeSort implements Serializable
     public static byte [] for_byteArray(byte arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        byte left_arr [] = null;
+        byte right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                byte left_arr [] = new byte [mid - low + 1];
-                byte right_arr [] = new byte [high - mid];
+                k = low;
+                left_arr = new byte [mid - low + 1];
+                right_arr = new byte [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -85,18 +88,21 @@ public class MergeSort implements Serializable
     public static Byte [] for_ByteArray(Byte arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        Byte left_arr [] = null;
+        Byte right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                Byte left_arr [] = new Byte [mid - low + 1];
-                Byte right_arr [] = new Byte [high - mid];
+                k = low;
+                left_arr = new Byte [mid - low + 1];
+                right_arr = new Byte [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -152,18 +158,21 @@ public class MergeSort implements Serializable
     public static short [] for_shortArray(short arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        short left_arr [] = null;
+        short right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                short left_arr [] = new short [mid - low + 1];
-                short right_arr [] = new short [high - mid];
+                k = low;
+                left_arr = new short [mid - low + 1];
+                right_arr = new short [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -219,18 +228,21 @@ public class MergeSort implements Serializable
     public static Short [] for_ShortArray(Short arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        Short left_arr [] = null;
+        Short right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                Short left_arr [] = new Short [mid - low + 1];
-                Short right_arr [] = new Short [high - mid];
+                k = low;
+                left_arr = new Short [mid - low + 1];
+                right_arr = new Short [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -286,18 +298,21 @@ public class MergeSort implements Serializable
     public static int [] for_intArray(int arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        int left_arr [] = null;
+        int right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
-                int i = 0, j = 0, k = low;
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                k = low;
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int left_arr [] = new int [mid - low + 1];
-                int right_arr [] = new int [high - mid];
+                left_arr = new int [mid - low + 1];
+                right_arr = new int [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -353,18 +368,21 @@ public class MergeSort implements Serializable
     public static Integer [] for_IntegerArray(Integer arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        Integer left_arr [] = null;
+        Integer right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                Integer left_arr [] = new Integer [mid - low + 1];
-                Integer right_arr [] = new Integer [high - mid];
+                k = low;
+                left_arr = new Integer [mid - low + 1];
+                right_arr = new Integer [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -420,18 +438,21 @@ public class MergeSort implements Serializable
     public static long [] for_longArray(long arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        long left_arr [] = null;
+        long right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                long left_arr [] = new long [mid - low + 1];
-                long right_arr [] = new long [high - mid];
+                k = low;
+                left_arr = new long [mid - low + 1];
+                right_arr = new long [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -487,18 +508,21 @@ public class MergeSort implements Serializable
     public static Long [] for_LongArray(Long arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        Long left_arr [] = null;
+        Long right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                Long left_arr [] = new Long [mid - low + 1];
-                Long right_arr [] = new Long [high - mid];
+                k = low;
+                left_arr = new Long [mid - low + 1];
+                right_arr = new Long [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -554,18 +578,21 @@ public class MergeSort implements Serializable
     public static float [] for_floatArray(float arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        float left_arr [] = null;
+        float right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                float left_arr [] = new float [mid - low + 1];
-                float right_arr [] = new float [high - mid];
+                k = low;
+                left_arr = new float [mid - low + 1];
+                right_arr = new float [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -621,18 +648,21 @@ public class MergeSort implements Serializable
     public static Float [] for_FloatArray(Float arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        Float left_arr [] = null;
+        Float right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                Float left_arr [] = new Float [mid - low + 1];
-                Float right_arr [] = new Float [high - mid];
+                k = low;
+                left_arr = new Float [mid - low + 1];
+                right_arr = new Float [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -688,18 +718,21 @@ public class MergeSort implements Serializable
     public static double [] for_doubleArray(double arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        double left_arr [] = null;
+        double right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                double left_arr [] = new double [mid - low + 1];
-                double right_arr [] = new double [high - mid];
+                k = low;
+                left_arr = new double [mid - low + 1];
+                right_arr = new double [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
@@ -755,18 +788,21 @@ public class MergeSort implements Serializable
     public static Double [] for_DoubleArray(Double arr [])
     {
 
+        int mid = 0, high = 0, i = 0, j = 0, k = 0;
+        Double left_arr [] = null;
+        Double right_arr [] = null;
         for (int factor = 1; factor <= arr.length - 1; factor *= 2)
         {
 
             for (int low = 0; low < arr.length - 1; low += 2*factor)
             {
 
-                int mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
-                int high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
+                mid = (low + factor - 1) < (arr.length - 1) ? (low + factor - 1) : (arr.length - 1);
+                high = (low + (2 * factor) - 1) < (arr.length - 1) ? (low + (2 * factor) - 1) : (arr.length - 1);
                 //System.out.println("factor : " + factor + " low : " + low + " mid : " + mid + " high : " + high);
-                int i = 0, j = 0, k = low;
-                Double left_arr [] = new Double [mid - low + 1];
-                Double right_arr [] = new Double [high - mid];
+                k = low;
+                left_arr = new Double [mid - low + 1];
+                right_arr = new Double [high - mid];
                 for (i = 0; i < left_arr.length; i += 1)
                 {
 
