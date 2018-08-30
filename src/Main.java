61d3fe5b15cd5ed8a67aa5a70e1d2a9ed7b7b507
Palash Sarkar <palashsarkar0007@gmail.com>
2018-08-30 23:47:42 +0530

    Testing 'QuickSort' algorithm class !!

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,8 @@
 import java.lang.Exception;
 
 import com.lib.algo.*;
+import com.lib.ds.LinkedList;
+import com.lib.ds.StackAsLinkedList;
 
 public class Main
 {
@@ -16,11 +18,13 @@ public class Main
     public static void main (String args []) throws Exception
     {
 
-        float arr [] = {3.66f, 5.87f, 13.22f, -26.33f, 1.30f, 44.33f, 56.65f, -14.43f, 1.09f, 2.67f, 7.22f};
+        //float arr [] = {3.66f, 5.87f, 13.22f, -26.33f, 1.30f, 44.33f, 56.65f, -14.43f, 1.09f, 2.67f, 7.22f};
         //int arr [] = {23, 11, 12, 5, 1, -2, 34, 55, 1, 2, 56};
-        //Double arr [] = {3.66, 5.87, 13.22, -26.33, 1.30, 44.33, 56.65, -14.43, 1.09, 2.67, 7.22};
-        arr = MergeSort.for_floatArray(arr);
-        for (float i : arr)
+        //Integer arr [] = {6, 6, 1, 3, 4, 4, 4, 56, 67, 13, 12, 56, 77, 899, 998, 899, 5, 5, 5, 17};
+        //Integer arr [] = {12};
+        Double arr [] = {3.66, 5.87, 13.22, -26.33, 1.30, 44.33, 56.65, -14.43, 1.09, 2.67, 7.22};
+        arr = QuickSort.for_DoubleArray(arr);
+        for (double i : arr)
         {
 
             System.out.print(i + " ");
