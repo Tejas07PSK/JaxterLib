86835a862d36755e7f7d26ed6924db39a9f64695
Palash Sarkar <palashsarkar0007@gmail.com>
2018-10-21 00:28:50 +0530

    Added Mathematics class with naive prime checker 'isPrime' function !!

---
+++ src/com/lib/algo/Mathematics.java
@@ -0,0 +1,38 @@
+/*
+
+        @Author : Tejas07PSK (Palash Sarkar),
+        @CreatedON : 20 Oct, 2018, 11:05 PM,
+        @File-Name : Mathematics.java
+
+ */
+
+package com.lib.algo;
+
+import java.io.Serializable;
+import java.lang.Math;
+
+public class Mathematics implements Serializable
+{
+
+    private static final long serialVersionUID = 1L;
+
+    public synchronized boolean isPrime(long n)
+    {
+
+        long limit = (long)Math.sqrt(n);
+        for (long i = 2; i <= limit; i += 1)
+        {
+
+            if ((n % i) == 0)
+            {
+
+                return (false);
+
+            }
+
+        }
+        return (true);
+
+    }
+
+}
