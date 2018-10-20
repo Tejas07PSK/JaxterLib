89a1e0a4d6452f0fb4f6c3c54399b1b05e7ce8a2
Palash Sarkar <palashsarkar0007@gmail.com>
2018-10-21 01:51:29 +0530

    Added Mathematics class with 'Sieve Of Eratosthenes' prime generator 'primeSieve' function !!

--- src/com/lib/algo/Mathematics.java
+++ src/com/lib/algo/Mathematics.java
@@ -10,6 +10,8 @@ package com.lib.algo;
 
 import java.io.Serializable;
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mathematics implements Serializable
 {
@@ -32,6 +34,60 @@ public class Mathematics implements Serializable
 
         }
         return (true);
+
+    }
+
+    public synchronized static List <Long> primeSieve(long n)
+    {
+
+        List <Boolean> temp =  new ArrayList <Boolean> ();
+        List <Long> lst = new ArrayList <Long> ();
+        long i = 0L, limit = (long)Math.sqrt(n);
+        for (; i <= n; i += 1)
+        {
+
+            if (i < 2)
+            {
+
+                temp.add(false);
+
+            }
+            else
+            {
+
+                temp.add(true);
+
+            }
+
+        }
+        for (i = 2; i <= limit; i += 1)
+        {
+
+            if (temp.get((int)i))
+            {
+
+                for (long j = 2; (j * i) <= n; j += 1)
+                {
+
+                    temp.set((int)(i * j), false);
+
+                }
+
+            }
+
+        }
+        for (i = 0; i <= n; i += 1)
+        {
+
+            if (temp.get((int)i))
+            {
+
+                lst.add(i);
+
+            }
+
+        }
+        return (lst);
 
     }
 
