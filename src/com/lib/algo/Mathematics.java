58da04026efb9fee176fa34717142faa2b64fa10
Palash Sarkar <palashsarkar0007@gmail.com>
2018-10-21 04:28:06 +0530

    Added Mathematics class with 'Segmented Sieve Of Eratosthenes' in-range prime generator 'segmentedPrimeSieve' function !!

--- src/com/lib/algo/Mathematics.java
+++ src/com/lib/algo/Mathematics.java
@@ -11,6 +11,7 @@ package com.lib.algo;
 import java.io.Serializable;
 import java.lang.Math;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Mathematics implements Serializable
@@ -18,7 +19,7 @@ public class Mathematics implements Serializable
 
     private static final long serialVersionUID = 1L;
 
-    public synchronized static boolean isPrime(long n)
+    public synchronized static boolean isPrime(final long n)
     {
 
         long limit = (long)Math.sqrt(n);
@@ -37,7 +38,7 @@ public class Mathematics implements Serializable
 
     }
 
-    public synchronized static List <Long> primeSieve(long n)
+    public synchronized static List <Long> primeSieve(final long n)
     {
 
         List <Boolean> temp =  new ArrayList <Boolean> ();
@@ -88,6 +89,107 @@ public class Mathematics implements Serializable
 
         }
         return (lst);
+
+    }
+
+    public synchronized static List <Long> segmentedPrimeSieve(final long start_end [], final long incl_excl [])
+    {
+
+        if ((start_end == null) || (start_end.length != 2) || (start_end[0] > start_end[1]))
+        {
+
+            return (Arrays.asList((new Long [] {0L})));
+
+        }
+        if ((incl_excl == null) || (incl_excl.length != 2) || ((incl_excl[0] > 1) || (incl_excl[0] < 0)) || ((incl_excl[1] > 1) || (incl_excl[1] < 0)))
+        {
+
+            return (Arrays.asList((new Long [] {0L})));
+
+        }
+        long limit = (long) (Math.floor(Math.sqrt(start_end[1])) + 1.0);
+        List <Long> primes = primeSieve(limit);
+        limit += 1;
+        long ori_prm_sz = primes.size();
+        long low = start_end[0], high = low + limit, i = 0, j = 0, lolim = 0;
+        boolean mark [] = new boolean [(int)(limit + 1)];
+        while (low < start_end[1])
+        {
+
+            refreshBoolArr(mark);
+            if (high > start_end[1])
+            {
+
+                high = start_end[1];
+
+            }
+            for (i = 0; i < ori_prm_sz; i += 1)
+            {
+
+                lolim = (int) ((Math.floor(low / primes.get((int)i)) * primes.get((int)i)));
+                if (lolim < low)
+                {
+
+                    lolim += primes.get((int)i);
+
+                }
+                for (j = lolim; j < high; j += primes.get((int)i))
+                {
+
+                    mark[(int) (j - low)] = false;
+
+                }
+
+            }
+            for (i = low; i < high; i += 1)
+            {
+
+                if (mark[(int)(i - low)])
+                {
+
+                    primes.add(i);
+
+                }
+
+            }
+            low = low + limit + 1;
+            high = low + limit;
+
+
+        }
+        while (primes.get(0) < start_end[0])
+        {
+
+            primes.remove(0);
+
+        }
+        if (incl_excl[0] == 0)
+        {
+
+            primes.remove(0);
+
+        }
+        if (incl_excl[1] == 0)
+        {
+
+            primes.remove(primes.size() - 1);
+
+        }
+        return (primes);
+
+    }
+
+    private synchronized static void refreshBoolArr(boolean arr [])
+    {
+
+        int i = 0, len = arr.length;
+        while (i < len)
+        {
+
+            arr[i] = true;
+            i += 1;
+
+        }
 
     }
 
